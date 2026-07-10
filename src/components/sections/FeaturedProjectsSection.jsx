91e9dc7ad54c993df08c1b9bae16e74643464c02
Adam_Diass <adamrpdiass@hotmail.com>
2026-07-10 11:44:47 -0300

    fix: id navigation

--- src/components/sections/FeaturedProjectsSection.jsx
+++ src/components/sections/FeaturedProjectsSection.jsx
@@ -105,7 +105,7 @@ function ProjectCard({ project, index }) {
 
 export function FeaturedProjectsSection() {
   return (
-    <section id="projetos" className="py-24 md:py-32 bg-surface">
+    <section id="dashboards" className="py-24 md:py-32 bg-surface">
       <div className="px-4 md:px-8 lg:px-12">
         <div className="max-w-7xl mx-auto">
           <motion.div
